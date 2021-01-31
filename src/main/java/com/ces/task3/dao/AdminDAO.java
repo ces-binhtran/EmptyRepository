145a7f745cd66758a271e3ef718b3ebb6dfecf9f
hieptran98w <hiep.tran@codeenginestudio.com>
2021-01-31 09:02:20 +0700

    delete some unnecessary class

--- src/main/java/com/ces/task3/dao/AdminDAO.java
+++
@@ -1,5 +0,0 @@
-package com.ces.task3.dao;
-import com.ces.task3.entity.AdminEntity;
-
-public interface AdminDAO extends BaseDAO<AdminEntity, Integer>{
-}
--- src/main/java/com/ces/task3/dao/impl/AdminDAOImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ces.task3.dao.impl;
-
-import com.ces.task3.dao.AdminDAO;
-import com.ces.task3.entity.AdminEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class AdminDAOImpl extends BaseDAOImpl<AdminEntity, Integer> implements AdminDAO{
-}
