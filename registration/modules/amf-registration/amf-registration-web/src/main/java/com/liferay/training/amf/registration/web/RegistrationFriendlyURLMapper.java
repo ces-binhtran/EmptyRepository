454d5336bbe0d3089e0ff00146de10a2a93409a6
sontruongces <son.truong@codeenginestudio.com>
2021-04-22 09:25:29 +0700

    add friendly url mapping

---
+++ registration/modules/amf-registration/amf-registration-web/src/main/java/com/liferay/training/amf/registration/web/RegistrationFriendlyURLMapper.java
@@ -0,0 +1,10 @@
+package com.liferay.training.amf.registration.web;
+
+public class RegistrationFriendlyURLMapper extends DefaultFriendlyURLMapper {
+    @Override
+    public String getMapping() {
+        return super.getMapping();
+    }
+
+    private static final String _MAPPING = "signUp";
+}
