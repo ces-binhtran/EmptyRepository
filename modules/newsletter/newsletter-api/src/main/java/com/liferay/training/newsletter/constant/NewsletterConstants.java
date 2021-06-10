a5d3410a0dabed82a22f6e9621b11e449732fda6
sontruongces <son.truong@codeenginestudio.com>
2021-06-10 13:40:44 +0700

    NP1.3 - Use ModelListener catch JournalArticle events, update data in the database

---
+++ modules/newsletter/newsletter-api/src/main/java/com/liferay/training/newsletter/constant/NewsletterConstants.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.training.newsletter.constant;
+
+/**
+ * @author Son Truong
+ */
+public class NewsletterConstants {
+
+    public static final String NEWSLETTER_ARTICLE = "Newsletter Article";
+    public static final String NEWSLETTER_ISSUE = "Newsletter Issue";
+}
